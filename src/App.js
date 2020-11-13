e9bfefb51192198a6da5f366cd7b18a88f76e6fa
Sarah Shealy <sshealy124@gmail.com>
2020-11-13 11:51:30 -0500

    edited home url for github pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
         <Nav />
         <Switch>
-          <Route exact path="/explore-copenhagen" render={() => (
+          <Route exact path="/" render={() => (
             <Home />
           )}>
           </Route>
