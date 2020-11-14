785c7ae87233b917a913a07386c27d31ba99abd2
Sarah Shealy <sshealy124@gmail.com>
2020-11-14 00:15:34 -0500

    removed tabs, imported link in the nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CovidData from "./pages/CovidData";
 import Forecast from "./pages/Forecast";
 import Nav from "./components/Navbar";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,11 @@ import { Link } from "react-router-dom";
 function Nav(){
     return(
         <AppBar>
-            <Tabs>
-                {/* <Tab href="/explore-copenhagen" label="Home"/>
-                <Tab href="/paths" label="Bicycle Paths"/>
-                <Tab href="/waste" label="Waste Basket Location"/>
-                <Tab href="/weather" label="Weather Forecast"/> */}
-                <Link to="/">Home</Link>
-                <Link to="/waste">Trash map</Link>
-                <Link to="paths">Bikes</Link>
-            </Tabs>
+            <Link to="/">Home</Link>
+            <Link to="/waste">Trash map</Link>
+            <Link to="paths">Bikes</Link>
         </AppBar>
+           
     )
 }
 
