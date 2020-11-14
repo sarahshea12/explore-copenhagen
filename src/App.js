4fe1332dba9e1e317fc676d9a47347412ed9e4bc
Sarah Shealy <sshealy124@gmail.com>
2020-11-13 23:30:39 -0500

    added basename to router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/explore-copenhagen">
         <Nav />
         <Switch>
           <Route exact path="/" render={() => (
