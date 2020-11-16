768a77604b786039ff1f9c51d7b1ad47d614618c
Sarah Shealy <sshealy124@gmail.com>
2020-11-16 16:39:46 -0500

    added arguments for weather api call function

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export default {
 
-    getForecast: function(){
+    getForecast: function(lat, lon){
         return axios({
             "method": "GET",
-            "url": "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=55.6761&lon=12.5683"
+            "url": "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=" + lat + "&lon=" + lon
         })
     },
-
+    
 }
